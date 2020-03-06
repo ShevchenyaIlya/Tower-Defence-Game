0194727eab70f01463d5430602b9692eadf34e32
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-03-07 02:00:07 +0300

    Add so called interfaces in project: IMovable and IUpgradable. (Not real interfaces, just tricking analogs.

--- towers/interfaces.py
+++ towers/interfaces.py
@@ -1,8 +1,22 @@
 from abc import ABCMeta, abstractmethod
 
 
-class ILocation(metaclass=ABCMeta):
+class ILocation:
     @abstractmethod
     def get_position(self):
         """Bring the location of object in window"""
+        pass
+
+
+class IMovable:
+    @abstractmethod
+    def move(self):
+        """Realizes in classes that have ability to change position by moving"""
+        pass
+
+
+class IUpgradable:
+    @abstractmethod
+    def upgrade(self):
+        """Change basic behavior of object throw upgraiding it"""
         pass
